f87d09058887dd81ffce16864f4955dc5472cabf
SenorKey <senorkey@gmail.com>
2026-03-27 01:07:05 -0400

    made some stylistic changes to etc page

--- js/etc.js
+++ js/etc.js
@@ -8,7 +8,7 @@
         {
             label: 'Main',
             title: 'First Content',
-            body: '<p class="body-text">This is the first piece of content. It starts in the main panel — the largest section on the left. Click one of the smaller panels on the right to cycle the stack forward.</p>'
+            body: '<p class="body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies blandit commodo. Ut nulla lorem, viverra eget accumsan et, convallis sed libero. Mauris ullamcorper vulputate congue. Suspendisse non imperdiet urna. Sed feugiat interdum condimentum. Sed semper nulla eget porta pretium. Cras at libero orci. Etiam congue commodo congue. Integer blandit sem ac dui rutrum, vitae eleifend est laoreet. Duis viverra laoreet turpis. Aliquam metus ex, viverra id laoreet eu, scelerisque quis tellus. Phasellus euismod commodo nisi ut dapibus. Phasellus lobortis quis orci sit amet bibendum. Praesent eget nibh eget augue euismod volutpat. Mauris et risus quis dolor sodales gravida. Maecenas condimentum pulvinar erat, eu lacinia dui gravida sit amet. Fusce lorem massa, scelerisque ut iaculis sed, placerat eu orci. Praesent tempor lectus et augue lobortis egestas. Phasellus vel tristique ex. Praesent nisi ipsum, volutpat sit amet suscipit a, mattis ut dolor. Donec aliquet consectetur purus et egestas. Pellentesque dui lorem, consequat vel mi sed, laoreet maximus nisi. Donec lobortis sit amet dolor eget fermentum. Maecenas bibendum nisi eu nisl ornare ullamcorper. Aliquam ex quam, ornare id sapien ut, gravida imperdiet nisl. Sed fringilla suscipit gravida. Quisque at nunc massa. Nullam in commodo dolor. Nunc risus felis, rhoncus quis vehicula vitae, blandit a nulla. Morbi sodales sem in felis volutpat, non vehicula eros imperdiet. Duis dictum, enim ac efficitur mattis, ipsum est rhoncus urna, at sagittis elit risus quis dui. Praesent at molestie elit, non consectetur quam. Pellentesque ac elit mauris. Suspendisse non vehicula urna, in eleifend nisl. Fusce lacinia ipsum eu lorem commodo pretium. Duis lacinia mauris eu risus blandit, quis iaculis nisl dapibus. Cras sodales nibh nec dolor semper ultricies. Cras sollicitudin congue leo, ac finibus arcu interdum sed. Ut mattis convallis sem eget finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut ac diam ut turpis pretium pretium eu sit amet sapien. Vivamus semper interdum ipsum id dictum. Quisque accumsan elit id dui bibendum, in placerat erat eleifend. Suspendisse sit amet lorem quis ex consequat faucibus sed quis mauris. Donec lacus justo, laoreet sagittis convallis ac, varius in quam. Donec eleifend eros justo, nec blandit est dignissim et. Integer sagittis suscipit consectetur.</p>'
         },
         {
             label: 'Stack item 2 of 4',
@@ -18,19 +18,19 @@
         {
             label: 'Stack item 3 of 4',
             title: 'Other Content 2',
-            body: '<p class="body-text">This is other content 2. It was sitting in the bottom sidebar — or maybe it was hidden entirely, waiting for its turn.</p>'
+            body: '<p class="body-text">Praesent at molestie elit, non consectetur quam. Pellentesque ac elit mauris. Suspendisse non vehicula urna, in eleifend nisl. Fusce lacinia ipsum eu lorem commodo pretium. Duis lacinia mauris eu risus blandit, quis iaculis nisl dapibus. Cras sodales nibh nec dolor semper ultricies. Cras sollicitudin congue leo, ac finibus arcu interdum sed. Ut mattis convallis sem eget finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut ac diam ut turpis pretium pretium eu sit amet sapien. Vivamus semper interdum ipsum id dictum. Quisque accumsan elit id dui bibendum, in placerat erat eleifend. Suspendisse sit amet lorem quis ex consequat faucibus sed quis mauris. Donec lacus justo, laoreet sagittis convallis ac, varius in quam. Donec eleifend eros justo, nec blandit est dignissim et. Integer sagittis suscipit consectetur.</p>'
         },
         {
             label: 'Stack item 4 of 4',
             title: 'Other Content 3',
-            body: '<p class="body-text">This is other content 3. It started out of view, but the stack brought it forward. There\'s nothing left after this one.</p>'
+            body: '<p class="body-text">In mattis eros et odio hendrerit, at finibus ex auctor. Vivamus molestie gravida turpis eu porta. Fusce convallis convallis porttitor. Ut quis odio volutpat, posuere tortor a, ultrices est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus ante ex, vulputate non hendrerit in, vestibulum in nunc. Quisque non fermentum tortor. Curabitur ullamcorper dui massa, quis volutpat dui viverra a. Sed nec augue ipsum. Phasellus ac libero vel lectus viverra aliquam sit amet non tortor. Mauris tortor diam, tempus quis tincidunt in, molestie vel quam. Pellentesque leo justo, pulvinar ac urna viverra, cursus venenatis lacus. Quisque sed mauris id mauris luctus porta ac eu nunc. Fusce in tempus elit. Quisque sit amet cursus metus. Ut sed dui metus.</p>'
         }
     ];
 
     var currentIndex = 0;
     var animating = false;
 
-    var mainEl = document.getElementById('etcMain');
+    var mainEl = document.getElementById('etcMainContent');
     var topEl = document.getElementById('etcTop');
     var bottomEl = document.getElementById('etcBottom');
     var shellEl = document.querySelector('.etc-shell');
